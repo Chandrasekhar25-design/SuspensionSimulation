d9d44e7ebbb51a13454ee07ae9ba129494d287f6
elliottwyse <elliottwyse@gmail.com>
2013-11-19 17:01:33 -0500

    suspensionSimulation works

--- suspensionSimulation.m
+++ suspensionSimulation.m
@@ -1,39 +1,56 @@
-function suspensionSimulation(steeringRange, verticalRange, varargin)
+function outputs = suspensionSimulation(steeringRange, verticalRange, varargin)
+    
+    %% Initialize variables and parse input arguments
+    %Initialize output structure
+    finish = struct();
 
-    if length(varargin) > 1
-        varName = varargin{1};
-        varRange = varargin{2};
+    %Parse variable input arguments
+    if length(varargin) == 1
+        geometries = varargin{1};
+        finish.outputs.dimensions = {'Steering', 'Compression', 'Geometries'};
+        finish.inputs.steeringRange = steeringRange;
+        finish.inputs.verticalRange = verticalRange;
+        finish.inputs.geometries = geometries;
     else
-        varName = [];
-        varRange = [1];
+        geometries = [0];
+        finish.outputs.dimensions = {'Steering', 'Compression'};
+        finish.inputs.steeringRange = steeringRange;
+        finish.inputs.verticalRange = verticalRange;
     end
+
+    %Initialize data matrices
+    camber = zeros(length(steeringRange),length(verticalRange),length(geometries));
+    rollAxis = zeros(length(steeringRange),length(verticalRange), length(geometries));
+    steeringAngle = zeros(length(steeringRange),length(verticalRange),length(geometries));
     
-    camber = zeros(length(steeringRange),length(verticalRange),length(varRange));
-    rollAxis = zeros(length(steeringRange),length(verticalRange), length(varRange));
-    steeringAngle = zeros(length(steeringRange),length(verticalRange),length(varRange));
-    
+    %% Use solve3D to generate data and then compute outputs
+    %For loops that iterate through the input parameters
     for p = 1:length(steeringRange)
         for i = 1:length(verticalRange)
-            for n = 1:length(varRange)
+            for n = 1:length(geometries)
+                
+                %If loops to determine whether or not to include additional
+                %inputs
                 if length(varargin)>1
-                    solution = solve3D(steeringRange(i),verticalRange(n),...
-                    [varName, '=', num2str(varRange(p))]);
+                    solution = solve3D(steeringRange(p),verticalRange(i),geometries(n));
                 else
-                    solution = solve3D(steeringRange(i),verticalRange(n));
+                    solution = solve3D(steeringRange(p),verticalRange(i));
                 end
                 
+                %Compute outputs
                 camber(p,i,n) = findCamber(solution);
-    
 %                 rollAxis(p,i,n) = findRoll(solution);
-    
                 steeringAngle(p,i,n) = findSteering(solution);
-                
-
                 
             end
         end
     end
     
-    genPlot(steeringAngle(:,:,1), steeringRange, verticalRange)
+    %% Output everything
+    finish.outputs.camber = camber;
+    finish.outputs.steeringAngle = steeringAngle;
+%     finish.outputs.rollAxis = rollAxis;
+
+    outputs = finish;
 
 end
